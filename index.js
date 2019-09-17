16c45bbfd62f75c3fd01ca535258a52f955a99e5
Denis Peshekhonov <denis@peshekhonov.ru>
2019-09-17 23:05:16 +0300

    set method

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/current', (req, res) => res.send('5|100|100|100|0'));
+let current = '5|100|100|100|0';
+app.get('/set', (req, res) => {
+    current = req.query.value;
+    res.send(`Set to: ${current}`);
+});
+app.get('/current', (req, res) => res.send(current));
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
